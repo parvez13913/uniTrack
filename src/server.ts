b075bf426e7023477cafedeb4b064bd6ac3281b3
parvez13913 <parvezz13913@gmail.com>
2023-06-03 19:22:17 +0600

    lint

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import config from "./config/index";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log(`Database is conneted successfull`);
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database is conneted successfull`)
     app.listen(config.port, () => {
-      console.log(`Application app listening on port ${config.port}`);
-    });
+      console.log(`Application app listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log("Fail to connect database", err);
+    console.log('Fail to connect database', err)
   }
 }
 
-bootstrap();
+bootstrap()
