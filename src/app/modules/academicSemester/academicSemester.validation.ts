27658b7bf64ea0e1ca7a33e81dd2e3b1237eba11
parvez13913 <parvezz13913@gmail.com>
2023-06-09 09:53:04 +0600

    [DONE] Create Academic Semester Constants

--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -1,54 +1,27 @@
 import { z } from 'zod';
+import {
+  academicSemesterCodes,
+  academicSemesterMonths,
+  academicSemesterTitles,
+} from './academicSemester.constant';
 
 const createAcademicSemesterZodSchema = z.object({
   body: z.object({
-    title: z.enum(['Autumn', 'Summar', 'Fall'], {
+    title: z.enum([...academicSemesterTitles] as [string, ...string[]], {
       required_error: 'Titel is Required',
     }),
     year: z.number({
       required_error: 'Year is Required',
     }),
-    code: z.enum(['01', '02', '03'], {
+    code: z.enum([...academicSemesterCodes] as [string, ...string[]], {
       required_error: 'Code is Required',
     }),
-    startMonth: z.enum(
-      [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-      ],
-      {
-        required_error: 'Start Month is needed',
-      }
-    ),
-    endMonth: z.enum(
-      [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-      ],
-      {
-        required_error: 'End Month is needed',
-      }
-    ),
+    startMonth: z.enum([...academicSemesterMonths] as [string, ...string[]], {
+      required_error: 'Start Month is needed',
+    }),
+    endMonth: z.enum([...academicSemesterMonths] as [string, ...string[]], {
+      required_error: 'End Month is needed',
+    }),
   }),
 });
 
