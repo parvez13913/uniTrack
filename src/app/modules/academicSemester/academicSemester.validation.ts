73062c0e78621c4ee89916cdac132f0c54239df6
parvez13913 <parvezz13913@gmail.com>
2023-06-12 15:54:09 +0600

    [DONE] Validation on update

--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -10,7 +10,7 @@ const createAcademicSemesterZodSchema = z.object({
     title: z.enum([...academicSemesterTitles] as [string, ...string[]], {
       required_error: 'Titel is Required',
     }),
-    year: z.number({
+    year: z.string({
       required_error: 'Year is Required',
     }),
     code: z.enum([...academicSemesterCodes] as [string, ...string[]], {
@@ -25,6 +25,46 @@ const createAcademicSemesterZodSchema = z.object({
   }),
 });
 
+const updatedAcademicSemesterZodSchema = z
+  .object({
+    body: z.object({
+      title: z
+        .enum([...academicSemesterTitles] as [string, ...string[]], {
+          required_error: 'Titel is Required',
+        })
+        .optional(),
+      year: z
+        .string({
+          required_error: 'Year is Required',
+        })
+        .optional(),
+      code: z
+        .enum([...academicSemesterCodes] as [string, ...string[]], {
+          required_error: 'Code is Required',
+        })
+        .optional(),
+      startMonth: z
+        .enum([...academicSemesterMonths] as [string, ...string[]], {
+          required_error: 'Start Month is needed',
+        })
+        .optional(),
+      endMonth: z
+        .enum([...academicSemesterMonths] as [string, ...string[]], {
+          required_error: 'End Month is needed',
+        })
+        .optional(),
+    }),
+  })
+  .refine(
+    data =>
+      (data.body.title && data.body.code) ||
+      (!data.body.title && !data.body.code),
+    {
+      message: 'Either both title and code should be provided or neither',
+    }
+  );
+
 export const AcademicSemesterValidation = {
   createAcademicSemesterZodSchema,
+  updatedAcademicSemesterZodSchema,
 };
