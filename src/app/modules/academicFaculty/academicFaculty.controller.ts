d01a93a2bf860709b22af7f6c9e71d6949c60c6e
parvez13913 <parvezz13913@gmail.com>
2023-06-13 16:50:31 +0600

    [DONE] Get All Academic Faculties

--- src/app/modules/academicFaculty/academicFaculty.controller.ts
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/paginationConstants';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { academicFacultyFilterableFields } from './academicFaculty.constants';
 import { IAcademicFaculty } from './academicFaculty.interface';
 import { AcademicFacultyService } from './academicFaculty.service';
 
@@ -19,6 +22,24 @@ const createFaculty = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllFaculties = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, academicFacultyFilterableFields);
+  const paginationOptions = pick(req.query, paginationFields);
+  const result = await AcademicFacultyService.getAllFaculties(
+    filters,
+    paginationOptions
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty retrieved Successfully !!',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const AcademicFacultyController = {
   createFaculty,
+  getAllFaculties,
 };
