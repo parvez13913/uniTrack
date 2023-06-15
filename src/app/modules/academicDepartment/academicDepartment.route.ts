022493df917bab7c04c10e2cc83171a17bffc885
parvez13913 <parvezz13913@gmail.com>
2023-06-15 13:58:20 +0600

    [DONE] Academic Department Validation

--- src/app/modules/academicDepartment/academicDepartment.route.ts
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -1,15 +1,26 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicDepartmentController } from './academicDepartment.controller';
+import { AcademicDepartmentValidation } from './academicDepartment.validation';
 
 const router = express.Router();
 
 router.post(
   '/create-department',
+  validateRequest(
+    AcademicDepartmentValidation.createAcademicDepartmentZodSchema
+  ),
   AcademicDepartmentController.createDepartment
 );
 router.get('/', AcademicDepartmentController.getAllDepartment);
 router.get('/:id', AcademicDepartmentController.getSingleDepartment);
-router.patch('/:id', AcademicDepartmentController.updateDepartment);
+router.patch(
+  '/:id',
+  validateRequest(
+    AcademicDepartmentValidation.updateAcademicDepartmentZodSchema
+  ),
+  AcademicDepartmentController.updateDepartment
+);
 router.delete('/:id', AcademicDepartmentController.deleteDepartment);
 
 export const AcademicDepartmentRoutes = router;
