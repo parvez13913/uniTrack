6a91dc660f4492680ff3dc56477f22ca5168304a
parvez13913 <parvezz13913@gmail.com>
2023-06-15 10:45:41 +0600

    [DONE] getAllDepartment

--- src/app/modules/academicDepartment/academicDepartment.controller.ts
+++ src/app/modules/academicDepartment/academicDepartment.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/paginationConstants';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { academicDepartmentFilterableFields } from './academicDepartment.constants';
 import { IAcademicDepartment } from './academicDepartment.interface';
 import { AcademicDepartmentService } from './academicDepartment.service';
 
@@ -19,6 +22,25 @@ const createDepartment = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllDepartment = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, academicDepartmentFilterableFields);
+  const paginationOptions = pick(req.query, paginationFields);
+
+  const result = await AcademicDepartmentService.getAllDepartment(
+    filters,
+    paginationOptions
+  );
+
+  sendResponse<IAcademicDepartment[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic departments fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const AcademicDepartmentController = {
   createDepartment,
+  getAllDepartment,
 };
--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -1,4 +1,12 @@
-import { IAcademicDepartment } from './academicDepartment.interface';
+import { SortOrder } from 'mongoose';
+import { PaginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import { academicDepartmentSearchableFields } from './academicDepartment.constants';
+import {
+  IAcademicDepartment,
+  IAcademicDepartmentFilters,
+} from './academicDepartment.interface';
 import { AcademicDepartment } from './academicDepartment.model';
 
 const createDepartment = async (
@@ -8,6 +16,59 @@ const createDepartment = async (
   return result;
 };
 
+const getAllDepartment = async (
+  filters: IAcademicDepartmentFilters,
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<IAcademicDepartment[]>> => {
+  const { limit, page, sortBy, sortOrder, skip } =
+    PaginationHelpers.calculatePagination(paginationOptions);
+  const { searchTerm, ...filtersData } = filters;
+  const andCondition = [];
+  if (searchTerm) {
+    andCondition.push({
+      $or: academicDepartmentSearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filtersData).length) {
+    andCondition.push({
+      $and: Object.entries(filtersData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    });
+  }
+
+  const sortConditions: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+
+  const wereConditions = andCondition.length > 0 ? { $and: andCondition } : {};
+  const result = await AcademicDepartment.find(wereConditions)
+    .populate('academicFaculty')
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await AcademicDepartment.countDocuments();
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const AcademicDepartmentService = {
   createDepartment,
+  getAllDepartment,
 };
