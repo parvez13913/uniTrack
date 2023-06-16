e5f7aa777ec0b95aca3eeb7b948b9d7ea8ce6825
parvez13913 <parvezz13913@gmail.com>
2023-06-16 07:14:15 +0600

    [DONE] Generate Student Id

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,17 +1,26 @@
-import { User } from './user.model'
+import { IAcademicSemester } from '../academicSemester/academicSemester.interface';
+import { User } from './user.model';
 
-export const findLastUserId = async () => {
+export const findLastStudentId = async () => {
   const lastUser = await User.findOne({}, { id: 1, _id: 0 })
     .sort({
       createdAt: -1,
     })
-    .lean()
+    .lean();
 
-  return lastUser?.id
-}
+  return lastUser?.id;
+};
 
-export const generateUserId = async () => {
-  const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
-  const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
-  return incrementedId
-}
+export const generateStudentId = async (
+  academicSemester: IAcademicSemester
+) => {
+  const currentId =
+    (await findLastStudentId()) || (0).toString().padStart(5, '0');
+  let incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+
+  incrementedId = `${academicSemester.year.substring(2)}${
+    academicSemester.code
+  }${incrementedId}`;
+
+  return incrementedId;
+};
