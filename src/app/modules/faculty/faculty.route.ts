9096c155d059c87a28ffd4265c98a8edd7d69783
parvez13913 <parvezz13913@gmail.com>
2023-06-24 15:52:43 +0600

    [DONE] update faculty

--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -5,5 +5,5 @@ const router = express.Router();
 
 router.get('/:id', FacultyController.getSingleFaculty);
 router.get('/', FacultyController.getAllFaculty);
-
+router.patch('/:id', FacultyController.updateFaculty);
 export const FacultyRoutes = router;
