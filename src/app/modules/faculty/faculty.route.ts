1150d4e77e3f4c4342bda7f043120483c9d4f38f
parvez13913 <parvezz13913@gmail.com>
2023-06-25 15:54:19 +0600

    [DONE] Faculty validation

--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { FacultyController } from './faculty.controller';
+import { FacultyValidation } from './faculty.validation';
 
 const router = express.Router();
 
 router.get('/:id', FacultyController.getSingleFaculty);
 router.get('/', FacultyController.getAllFaculty);
-router.patch('/:id', FacultyController.updateFaculty);
+router.patch(
+  '/:id',
+  validateRequest(FacultyValidation.updateFacultyZodSchema),
+  FacultyController.updateFaculty
+);
 export const FacultyRoutes = router;
