e793d4e729c1e92ba6f376ee2b1a3f0c16466654
parvez13913 <parvezz13913@gmail.com>
2023-07-10 19:35:50 +0600

    [DONE] create management department

---
+++ src/app/modules/managementDepartment/managementDepartment.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { IManagementDepartment } from './managementDepartment.interface';
+import { ManagementDepartmentService } from './managementDepartment.service';
+
+const createDepartment = catchAsync(async (req: Request, res: Response) => {
+  const { ...managementDepartmentData } = req.body;
+  const result = await ManagementDepartmentService.createDepartment(
+    managementDepartmentData
+  );
+
+  sendResponse<IManagementDepartment>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Management Department created successfully',
+    data: result,
+  });
+});
+
+export const ManagementDepartmentController = {
+  createDepartment,
+};
