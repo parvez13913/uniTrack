d03e0175ca9b7ba50c25707083eafc461436bad1
parvez13913 <parvezz13913@gmail.com>
2023-07-17 20:40:45 +0600

    [DONE] Create accessToken and refreshToken

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,26 +1,47 @@
 import httpStatus from 'http-status';
+import { Secret } from 'jsonwebtoken';
+import config from '../../../config';
 import ApiError from '../../../errors/ApiErrors';
+import { JwtHelpers } from '../../../helpers/jwtHelpers';
 import { User } from '../user/user.model';
-import { ILoginUser } from './auth.interface';
+import { ILoginUser, ILoginUserResponse } from './auth.interface';
 
-const loginUser = async (payload: ILoginUser) => {
+const loginUser = async (
+  payload: ILoginUser
+): Promise<ILoginUserResponse | null> => {
   const { id, password } = payload;
-
-  //   check user exist
-  const user = new User();
-  const isUserExist = await user.isUserExist(id);
+  const isUserExist = await User.isUserExist(id);
   if (!isUserExist) {
     throw new ApiError(httpStatus.NOT_FOUND, 'User does not Exist');
   }
 
   // Match password
-
   if (
     isUserExist.password &&
-    !user.isPasswordMatched(password, isUserExist?.password)
+    !(await User.isPasswordMatched(password, isUserExist?.password))
   ) {
     throw new ApiError(httpStatus.UNAUTHORIZED, 'Password is incorrect');
   }
+
+  // access token && refresh token
+  const { id: userId, role, needsPasswordChange } = isUserExist;
+  const accessToken = JwtHelpers.createToken(
+    { userId, role },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+
+  const refreshToken = JwtHelpers.createToken(
+    { userId, role },
+    config.jwt.refresh_secret as Secret,
+    config.jwt.refresh_expires_in as string
+  );
+
+  return {
+    needsPasswordChange,
+    accessToken,
+    refreshToken,
+  };
 };
 
 export const AuthService = {
--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -14,12 +14,17 @@ export type IUser = {
   admin?: Types.ObjectId | IAdmin;
 };
 
-export type UserMethods = {
-  isUserExist(id: string): Promise<Partial<IUser> | null>;
+export type UserModel = {
+  isUserExist(
+    id: string
+  ): Promise<Pick<
+    IUser,
+    'id' | 'password' | 'needsPasswordChange' | 'role'
+  > | null>;
   isPasswordMatched(
     givenPassword: string,
     savedPassword: string
   ): Promise<boolean>;
-};
+} & Model<IUser>;
 
-export type UserModel = Model<IUser, Record<string, unknown>, UserMethods>;
+// export type UserModel = Model<IUser, Record<string, unknown>, UserMethods>;
